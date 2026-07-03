f6e1cc56cd4725a02d3e0722ce064f8ac5c5141f
Aniekan Victory <trovic101@gmail.com>
2026-07-03 13:26:54 +0100

    style: extract typography localization mapping block 6

--- src/lib/helpers/ui.ts
+++ src/lib/helpers/ui.ts
@@ -3,3 +3,4 @@ export const getThemeClass_2 = () => 'dark';
 export const getThemeClass_3 = () => 'dark';
 export const getThemeClass_4 = () => 'dark';
 export const getThemeClass_5 = () => 'dark';
+export const getThemeClass_6 = () => 'dark';
