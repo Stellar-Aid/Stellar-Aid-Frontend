26e2b4a38fddf43cf6b715dd4c66c53bc2840d8c
Aniekan Victory <trovic101@gmail.com>
2026-07-03 13:27:05 +0100

    style: fix minor button focus states and hover transitions 7

--- src/components/micro/index.ts
+++ src/components/micro/index.ts
@@ -4,3 +4,4 @@ export const ButtonFocus_3 = () => 'Focus';
 export const ButtonFocus_4 = () => 'Focus';
 export const ButtonFocus_5 = () => 'Focus';
 export const ButtonFocus_6 = () => 'Focus';
+export const ButtonFocus_7 = () => 'Focus';
