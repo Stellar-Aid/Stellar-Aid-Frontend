b651162d4388ab1b9a8c6be628394bfe5afffde8
Aniekan Victory <trovic101@gmail.com>
2026-07-15 13:35:05 +0100

    ci: document edge case handling in input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -19,3 +19,5 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
 Input.displayName = "Input";
 
 export { Input };
+
+// TODO: Review performance constraints here (Ref: 0535897a - 1784118905)
