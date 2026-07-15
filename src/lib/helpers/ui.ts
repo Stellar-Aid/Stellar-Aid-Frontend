ce6153f89cd0369cfb7bd565f03f6553376091c4
Aniekan Victory <trovic101@gmail.com>
2026-07-15 13:35:26 +0100

    style: tag potential tech debt in ui.ts

--- src/lib/helpers/ui.ts
+++ src/lib/helpers/ui.ts
@@ -8,3 +8,5 @@ export const getThemeClass_7 = () => 'dark';
 export const getThemeClass_8 = () => 'dark';
 export const getThemeClass_9 = () => 'dark';
 export const getThemeClass_10 = () => 'dark';
+
+// TODO: Review performance constraints here (Ref: ca42929b - 1784118925)
